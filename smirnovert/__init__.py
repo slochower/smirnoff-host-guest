a12fbc4d82b92f609da362e116564364486aa2eb
David Slochower <slochower@gmail.com>
2018-03-16 19:12:39 -0700

    Add `git` for version control

--- smirnovert/__init__.py
+++ smirnovert/__init__.py
@@ -1,1 +1,13 @@
+import subprocess as sp
+import re as re
+
 __version__ = '0.0.4'
+try:
+    # Try to use git to find current commit.
+    p = sp.Popen(
+        ["git", "describe", "--always"], stdout=sp.PIPE, stderr=sp.PIPE)
+    output, error = p.communicate()
+    git_describe = output.decode("utf-8").strip()
+    __version__ = re.sub('-g[0-9a-f]*$', '', git_describe)
+except sp.CalledProcessError as e:
+    pass
