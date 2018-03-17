ea17852ca64127340baec2731ab7427ced8d9407
David Slochower <slochower@gmail.com>
2018-03-16 19:30:51 -0700

    Adds a print statement about versioning

--- smirnovert/__init__.py
+++ smirnovert/__init__.py
@@ -4,10 +4,13 @@ import re as re
 __version__ = '0.0.4'
 try:
     # Try to use git to find current commit.
+    print('Using `git` to find current version`.')
     p = sp.Popen(
         ["git", "describe", "--always"], stdout=sp.PIPE, stderr=sp.PIPE)
     output, error = p.communicate()
     git_describe = output.decode("utf-8").strip()
-    __version__ = re.sub('-g[0-9a-f]*$', '', git_describe)
+    # __version__ = re.sub('-g[0-9a-f]*$', '', git_describe)
+    __version__ = git_describe
 except sp.CalledProcessError as e:
+    print('Could not use `git` to find version.')
     pass
