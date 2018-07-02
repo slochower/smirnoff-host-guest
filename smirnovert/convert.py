869a97b9782fcf80a221e9e4365b3a1e63aefe40
David Slochower <slochower@gmail.com>
2018-07-02 10:55:16 -0700

    Adds support for a source directory separate from destination. Uses `os.path` for directories.

--- smirnovert/convert.py
+++ smirnovert/convert.py
@@ -12,19 +12,25 @@ import glob
 import os
 
 
-def convert(destination,
-            prefix,
-            reference_prmtop,
-            reference_inpcrd,
-            host_resname,
-            guest_resname,
-            debug=False):
+def convert(
+    source,
+    destination,
+    prefix,
+    reference_prmtop,
+    reference_inpcrd,
+    host_resname,
+    guest_resname,
+    dummy=False,
+    debug=False,
+):
     """
     Convert from an existing parameter set to SMIRNOFF99Frosst.
     Parameters
     ----------
+    source : str
+        Directory where existing files will be read
     destination : str
-        Directory where existing files will be read and new files will be written
+        Directory where new files will be written
     prefix : str
         Base name of output files (e.g., "smirnoff" or "hg")
     reference_prmtop : str
@@ -35,6 +41,8 @@ def convert(destination,
         Residue name of the host molecule (*not* the mask)
     guest_resname : str
         Residue name of the guest molecule (*not* the mask)
+    dummy : bool
+        Whether the reference structures include dummy atoms
     debug : bool
         If True, intermediary files will be kept
 
@@ -45,120 +53,144 @@ def convert(destination,
     """
 
     clean_up(
-        destination=destination,
-        host_resname=host_resname,
-        guest_resname=guest_resname)
+        destination=destination, host_resname=host_resname, guest_resname=guest_resname
+    )
     reference = pmd.load_file(
-        destination + reference_prmtop, xyz=destination + reference_inpcrd)
+        os.path.join(source, reference_prmtop),
+        xyz=os.path.join(source, reference_inpcrd),
+    )
 
     create_pdb_with_conect(
-        solvated_pdb=destination + reference_inpcrd,
-        amber_prmtop=destination + reference_prmtop,
-        output_pdb=destination + prefix + '.pdb')
+        solvated_pdb=os.path.join(destination, reference_inpcrd),
+        amber_prmtop=os.path.join(destination, reference_prmtop),
+        output_pdb=os.path.join(destination, prefix) + ".pdb",
+    )
 
     prune_conect(
-        input_pdb=prefix + '.pdb',
-        output_pdb=prefix + '.pruned.pdb',
-        path=destination)
+        input_pdb=prefix + ".pdb", output_pdb=prefix + ".pruned.pdb", path=destination
+    )
 
-    components = split_topology(file_name=destination + prefix + '.pruned.pdb')
+    components = split_topology(
+        file_name=os.path.join(destination, prefix) + ".pruned.pdb"
+    )
     hg_topology = create_host_guest_topology(
-        components, host_resname=host_resname, guest_resname=guest_resname)
+        components, host_resname=host_resname, guest_resname=guest_resname
+    )
 
     create_host_mol2(
-        solvated_pdb=destination + prefix + '.pruned.pdb',
-        amber_prmtop=destination + reference_prmtop,
+        solvated_pdb=os.path.join(destination, prefix) + ".pruned.pdb",
+        amber_prmtop=os.path.join(destination, reference_prmtop),
         mask=host_resname,
-        output_mol2=destination + host_resname + '.mol2')
+        output_mol2=os.path.join(destination, host_resname) + ".mol2",
+    )
 
     create_host_mol2(
-        solvated_pdb=destination + prefix + '.pdb',
-        amber_prmtop=destination + reference_prmtop,
+        solvated_pdb=os.path.join(destination, prefix) + ".pdb",
+        amber_prmtop=os.path.join(destination, reference_prmtop),
         mask=guest_resname,
-        output_mol2=destination + guest_resname + '.mol2')
+        output_mol2=os.path.join(destination, guest_resname) + ".mol2",
+    )
 
     convert_mol2_to_sybyl_antechamber(
-        input_mol2=destination + host_resname + '.mol2',
-        output_mol2=destination + host_resname + '-sybyl.mol2',
-        ac_doctor=False)
+        input_mol2=os.path.join(destination, host_resname) + ".mol2",
+        output_mol2=os.path.join(destination, host_resname) + "-sybyl.mol2",
+        ac_doctor=False,
+    )
 
     convert_mol2_to_sybyl_antechamber(
-        input_mol2=destination + guest_resname + '.mol2',
-        output_mol2=destination + guest_resname + '-sybyl.mol2',
-        ac_doctor=False)
+        input_mol2=os.path.join(destination, guest_resname) + ".mol2",
+        output_mol2=os.path.join(destination, guest_resname) + "-sybyl.mol2",
+        ac_doctor=False,
+    )
 
     extract_water_and_ions(
         amber_prmtop=reference_prmtop,
         amber_inpcrd=reference_inpcrd,
-        host_residue=':' + host_resname,
-        guest_residue=':' + guest_resname,
+        host_residue=":" + host_resname,
+        guest_residue=":" + guest_resname,
         dummy_atoms=True,
-        output_pdb='water_ions.pdb',
-        path=destination)
+        output_pdb="water_ions.pdb",
+        path=destination,
+    )
 
-    create_water_and_ions_parameters(
-        input_pdb='water_ions.pdb',
-        output_prmtop='water_ions.prmtop',
-        output_inpcrd='water_ions.inpcrd',
-        dummy_atoms=False,
-        path=destination)
+    if not dummy:
+        create_water_and_ions_parameters(
+            input_pdb="water_ions.pdb",
+            output_prmtop="water_ions.prmtop",
+            output_inpcrd="water_ions.inpcrd",
+            dummy_atoms=False,
+            path=destination,
+        )
+    else:
+        create_water_and_ions_parameters(
+            input_pdb="water_ions.pdb",
+            output_prmtop="water_ions.prmtop",
+            output_inpcrd="water_ions.inpcrd",
+            dummy_atoms=True,
+            path=destination,
+        )
 
     host = load_mol2(
-        filename=destination + host_resname + '-sybyl.mol2',
+        filename=os.path.join(destination, host_resname) + "-sybyl.mol2",
         name=host_resname,
-        add_tripos=True)
+        add_tripos=True,
+    )
 
     guest = load_mol2(
-        filename=destination + guest_resname + '-sybyl.mol2',
+        filename=os.path.join(destination, guest_resname) + "-sybyl.mol2",
         name=guest_resname,
-        add_tripos=False)
+        add_tripos=False,
+    )
 
     check_unique_atom_names(host)
     check_unique_atom_names(guest)
     molecules = [host, guest]
 
-    ff = ForceField('forcefield/smirnoff99Frosst.offxml')
+    ff = ForceField("forcefield/smirnoff99Frosst.offxml")
     system = ff.createSystem(
         hg_topology.topology,
         molecules,
         nonbondedCutoff=1.1 * unit.nanometer,
-        ewaldErrorTolerance=1e-4)
+        ewaldErrorTolerance=1e-4,
+    )
 
     hg_structure = pmd.openmm.topsystem.load_topology(
-        hg_topology.topology, system, hg_topology.positions)
+        hg_topology.topology, system, hg_topology.positions
+    )
 
     check_bond_lengths(hg_structure, threshold=4)
 
     try:
-        hg_structure.save(destination + 'hg.prmtop')
+        hg_structure.save(os.path.join(destination, "hg.prmtop"))
     except OSError:
-        print('Check if the host-guest parameter file already exists...')
+        print("Check if the host-guest parameter file already exists...")
 
     try:
-        hg_structure.save(destination + 'hg.inpcrd')
+        hg_structure.save(os.path.join(destination, "hg.inpcrd"))
     except OSError:
-        print('Check if the host-guest coordinate file already exists...')
+        print("Check if the host-guest coordinate file already exists...")
 
     water_and_ions = pmd.amber.AmberParm(
-        destination + 'water_ions.prmtop',
-        xyz=destination + 'water_ions.inpcrd')
+        destination + "water_ions.prmtop", xyz=destination + "water_ions.inpcrd"
+    )
 
     merged = mergeStructure(hg_structure, water_and_ions)
     merged.box = reference.box
     try:
-        merged.save(destination + 'smirnoff.prmtop')
+        merged.save(os.path.join(destination, "smirnoff.prmtop"))
     except:
-        print('Check if solvated parameter file already exists...')
+        print("Check if solvated parameter file already exists...")
     try:
-        merged.save(destination + 'smirnoff.inpcrd')
+        merged.save(os.path.join(destination, "smirnoff.inpcrd"))
     except:
-        print('Check if solvated coordinate file already exists...')
+        print("Check if solvated coordinate file already exists...")
 
     if not debug:
         clean_up(
             destination=destination,
             host_resname=host_resname,
-            guest_resname=guest_resname)
+            guest_resname=guest_resname,
+        )
 
     return merged
 
@@ -177,14 +209,14 @@ def clean_up(destination, host_resname, guest_resname):
 
 
     """
-    water = glob.glob(destination + 'water_ions*')
-    inpt = glob.glob(destination + '*.pdb.in')
-    outp = glob.glob(destination + '*.pdb.out')
-    host = glob.glob(destination + host_resname + '*')
-    guest = glob.glob(destination + guest_resname + '*')
+    water = glob.glob(os.path.join(destination, "water_ions") + "*")
+    inpt = glob.glob(os.path.join(destination) + "*.pdb.in")
+    outp = glob.glob(os.path.join(destination) + "*.pdb.out")
+    host = glob.glob(os.path.join(destination, host_resname) + "*")
+    guest = glob.glob(os.path.join(destination, guest_resname) + "*")
     for file in water + host + guest + inpt + outp:
         try:
-            print(f'Removing {file}...')
+            print(f"Removing {file}...")
             os.remove(file)
         except:
             pass
